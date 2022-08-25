7124daf18167972e8f3209bdc87948d3d0fc10ed
RaulAlcaniz <ralcaniz83@gmail.com>
2022-08-25 12:11:16 +0100

    Add test file

---
+++ food_chain_test.rb
@@ -0,0 +1,20 @@
+
+#!/usr/bin/env ruby
+require 'minitest/autorun'
+require 'pry'
+require_relative 'food_chain'
+
+class FoodChainTest < Minitest::Test
+  # This test is an acceptance test.
+  #
+  # If you find it difficult to work the problem with so much
+  # output, go ahead and add a `skip`, and write whatever
+  # unit tests will help you. Then unskip it again
+  # to make sure you got it right.
+  def test_the_whole_song
+    song_file = File.expand_path('../song.txt', __FILE__)
+    expected  = IO.read(song_file)
+
+    assert_equal expected, FoodChain2.new.song
+  end
+end
